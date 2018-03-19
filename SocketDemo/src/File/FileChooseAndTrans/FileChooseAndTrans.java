b3611ca2d9f7137e58cdef261c7731c6052c4a7f
DossShi <995025784@qq.com>
2018-03-19 16:19:13 +0800

    多文件传输测试 File.multiTransfer

--- SocketDemo/src/File/FileChooseAndTrans/FileChooseAndTrans.java
+++ SocketDemo/src/File/FileChooseAndTrans/FileChooseAndTrans.java
@@ -18,8 +18,9 @@ import javax.swing.JFileChooser;
  */
 public class FileChooseAndTrans extends Socket{
 	
-	private static final String SERVER_IP = "192.168.0.6";	//服务器端IP
-	private static final int SERVER_PORT = 8899;	//服务器端端口
+	private static final String SERVER_IP = "192.168.0.7";	//服务器端IP
+//	private static final String SERVER_IP = "127.0.0.1";	//服务器端IP
+	private static final int SERVER_PORT = 8889;	//服务器端端口
 
 	private Socket client;
 	
@@ -47,7 +48,18 @@ public class FileChooseAndTrans extends Socket{
 		chooser.showOpenDialog(null);
 		File f = chooser.getSelectedFile();
 		return f;
-		
+	}
+	
+	/**
+	 * 通过窗口选择文件（可多选）
+	 * @return 选择的文件列表
+	 */
+	public static File[] getFiles() {
+		JFileChooser chooser = new JFileChooser();
+		chooser.setMultiSelectionEnabled(true);
+		chooser.showOpenDialog(null);
+		File[] fs = chooser.getSelectedFiles();
+		return fs;
 	}
 	
 	/**
@@ -97,13 +109,66 @@ public class FileChooseAndTrans extends Socket{
 	}
 	
 	/**
+	 * 向服务器传送文件列表
+	 * @throws Exception
+	 */
+	public void sendFiles() throws Exception {
+		try {
+			File[] files = getFiles();
+			if (files.length <= 0) {
+				return ;
+			}
+			dos = new DataOutputStream(client.getOutputStream());
+			dos.writeInt(dos.size());
+			dos.flush();
+			for (File file : files) {
+				if (file.exists()) {
+					fis = new FileInputStream(file);
+					
+					
+					//文件名和长度
+					dos.writeUTF(file.getName());
+					dos.flush();
+					dos.writeLong(file.length());
+					dos.flush();
+					
+					//开始传输文件
+					System.out.println("========" + file.getName() + " 开始传输文件==========");
+					byte[] bytes = new byte[1024];
+					int length = 0;
+					long progress = 0;
+					while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
+						dos.write(bytes, 0, length);
+						dos.flush();
+						progress += length;
+						if (100 * progress / file.length() > 100*(progress-length)/file.length()) {
+							System.out.print("| " + (100*progress/file.length() + "% |"));
+						}
+					}
+					System.out.println();
+					System.out.println("========== " + file.getName() + " 文件传输成功===========");
+				}
+			}
+		}  catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) 
+				fis.close();
+			if (dos != null) 
+				dos.close();
+			client.close();
+		}
+	}
+	
+	/**
 	 * 入口
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
 			FileChooseAndTrans client = new FileChooseAndTrans();	//启动客户端连接
-			client.sendFile();	//传输文件
+//			client.sendFile();	//传输文件
+			client.sendFiles(); //传送文件列表
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
