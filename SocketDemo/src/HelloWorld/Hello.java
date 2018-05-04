599ec6819e225bbe42e9eb053ae9b10a4db5bb87
DossShi <995025784@qq.com>
2018-05-04 10:02:38 +0800

    扫描并获取指定目录的内容

--- SocketDemo/src/HelloWorld/Hello.java
+++ SocketDemo/src/HelloWorld/Hello.java
@@ -1,9 +1,17 @@
 package HelloWorld;
 
+import java.io.File;
+import java.util.List;
+
+import File.fileScanner.FileScanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java Socket Demo");
+		
+		List<File> fileList = FileScanner.getFiles("D:\\Project\\SocketTest\\FileInput");
+		System.out.println(fileList.size());
 	}
 
 }
